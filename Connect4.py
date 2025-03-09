b40d717e5bef4bd447eb891049edbd30ceac6ed1
AlexwardSHU <100145354+AlexwardSHU@users.noreply.github.com>
2025-03-09 12:03:54 +0000

    Update Connect4.py
    ai agent work

--- Connect4.py
+++ Connect4.py
@@ -18,7 +18,6 @@ class Connect4:
             for col in range(7):
                 if self.board[row, col] == ' ':
                     legal_moves.append(col)
-        print(legal_moves)
         return legal_moves
 
     def make_move(self, col, player):
@@ -53,29 +52,56 @@ class Connect4:
     def draw(self):
         return ' ' not in self.board
 
-def play():
+class Random_Agent:
+    def __init__(self, game):
+        self.game = game
+        self.AI_Player = "Y"
+        self.Human_Player = "R"
+
+    def Random_Move(self):
+        return random.randint(0, 6)
+
+def play(opponent):
     game = Connect4()
+    agent = Random_Agent(game)
+    match opponent:
+        case 1: agent = Random_Agent(game)
     valid = False
+    AIvalid = False
     print("Red vs Yellow")
     while True:
         game.print_board()
         print(game.currentPlayer + "'s turn")
-        while not valid:
-            try:
-                col = int(input("Pick a column(0-6): "))
-                if col in game.find_legal_moves():
-                    if game.make_move(col, game.currentPlayer):
-                        valid = True
+        if game.currentPlayer == "R" or opponent == 5:
+            while not valid:
+                try:
+                    col = int(input("Pick a column(0-6): "))
+                    if col in game.find_legal_moves():
+                        if game.make_move(col, game.currentPlayer):
+                            valid = True
+                        else:
+                            print("Column Full")
                     else:
-                        print("Column Full")
-                else:
+                        print("Invalid input, try again")
+                except ValueError:
                     print("Invalid input, try again")
-            except ValueError:
-                print("Invalid input, try again")
 
         if game.check_for_win(game.currentPlayer):
             game.print_board()
             print(game.currentPlayer + " wins")
+            break
+
+        if opponent == 1:
+            while not AIvalid:
+                col = agent.Random_Move()
+                print(col)
+                if col in game.find_legal_moves():
+                    if game.make_move(col, agent.AI_Player):
+                        AIvalid = True
+
+        if game.check_for_win(agent.AI_Player):
+            game.print_board()
+            print(agent.AI_Player + " wins")
             break
 
         if game.draw():
@@ -83,11 +109,29 @@ def play():
             print("Draw")
             break
 
-        if game.currentPlayer == 'R':
-            game.currentPlayer = 'Y'
-        else:
-            game.currentPlayer = 'R'
+        if opponent == 5:
+            if game.currentPlayer == 'R':
+                game.currentPlayer = 'Y'
+            else:
+                game.currentPlayer = 'R'
         valid = False
+        AIvalid = False
 
-play()
-print("bingus dingus")
+def main_menu():
+    valid = False
+    choice = 0
+    print("Connect 4")
+    print("1) Random Agent \n 2) Rule Based Agent \n 3) Minimax Agent \n 4) ML Agent \n 5) P1 VS P2")
+    while not valid:
+        try:
+            choice = int(input("Please choose a Game Mode: "))
+            if 6 > choice > 0:
+                valid = True
+            else:
+                print("Invalid input, try again")
+        except ValueError:
+            print("Invalid input, try again")
+    return choice
+
+player = main_menu()
+play(player)
